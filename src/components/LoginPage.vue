c3b2ec78bd7b9c9e871e5c3756cee4eb30485416
kjgarza <kgarza@datacite.org>
2023-04-01 11:29:11 +0200

    tailwinds look

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,8 +1,16 @@
 // eslint-disable-next-line vue/multi-word-component-names
 <template>
-  <div class="bg-white p-6 rounded-lg shadow-md">
-    <h2 class="text-lg font-medium mb-4">Login</h2>
-    <form @submit.prevent="submitLogin">
+  <div class="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
+  <div class="w-full max-w-md space-y-8">
+    <div>
+      <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+      <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+      <p class="mt-2 text-center text-sm text-gray-600">
+        Or
+        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
+      </p>
+    </div>
+    <form class="mt-8 space-y-6" action="#"  @submit.prevent="submitLogin">
       <label class="block mb-2 font-bold" for="client-id">Client ID</label>
       <input class="w-full p-2 mb-4 border rounded" id="client-id" v-model="username" required>
       <label class="block mb-2 font-bold" for="password">Password</label>
@@ -10,6 +18,7 @@
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Login</button>
     </form>
   </div>
+</div>
 </template>
 
 
--- src/components/SubmitInput.vue
+++ src/components/SubmitInput.vue
@@ -1,19 +1,40 @@
 <!-- src/components/SubmitInput.vue -->
 
+
 <template>
-  <form class="w-full max-w-sm" @submit.prevent="submitInput">
-    <div class="flex flex-wrap mb-6">
-      <label for="title" class="block text-gray-700 font-bold mb-2">Title</label>
-      <input id="title" v-model="inputValue" class="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline" type="text" placeholder="Enter title" required>
+  <form class="max-w-4xl mx-auto px-4 py-8" @submit.prevent="submitInput">
+  <div class="space-y-12">
+    <div class="border-b border-gray-900/10 pb-12">
+      <h2 class="text-base font-semibold leading-7 text-gray-900">Profile</h2>
+      <p class="mt-1 text-sm leading-6 text-gray-600">This information will be displayed publicly so be careful what you share.</p>
+    
+      <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+        <div class="sm:col-span-4">
+          <label for="apiKey" class="block text-sm font-medium leading-6 text-gray-900">Api Key</label>
+          <div class="mt-2">
+            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+              <input type="password" v-model="apiKey" name="apiKey" id="apiKey" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
+            </div>
+          </div>
+        </div>
+
+        <div class="col-span-full">
+          <label for="inputValue" class="block text-sm font-medium leading-6 text-gray-900">About</label>
+          <div class="mt-2">
+            <textarea id="inputValue" v-model="inputValue" name="inputValue" rows="3" class="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"></textarea>
+          </div>
+          <p class="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about your resource</p>
+        </div>
+      </div>
     </div>
-    <div class="flex flex-wrap mb-6">
-      <label for="apiKey" class="block text-gray-700 font-bold mb-2">apiKey</label>
-      <input id="apiKey" v-model="apiKey" class="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline" type="password" placeholder="Enter token" required>
-    </div>
-    <div class="flex flex-wrap mb-6">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Submit</button>
-    </div>
-  </form>
+
+  </div>
+
+  <div class="mt-6 flex items-center justify-end gap-x-6">
+    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Submit</button>
+  </div>
+</form>
+
 </template>
 
 
