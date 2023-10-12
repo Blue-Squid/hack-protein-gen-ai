64a4c215f0300647a7664499d0218dfb9e8ab851
Kristian Garza <kj.garza@gmail.com>
2023-10-12 23:59:22 +0200

    hugging face fixes

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,48 @@
 const { defineConfig } = require('@vue/cli-service')
+const webpack = require("webpack");
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        "fs": false,
+        "tls": false,
+        "net": false,
+        "path": false,
+        "zlib": false,
+        "http": false,
+        "https": false,
+        "stream": false,
+        "crypto": false,
+        "url": false,
+        // "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
+      } 
+    },
+    plugins: [
+      // new webpack.ProvidePlugin({
+      //   process: "process/browser",
+      //   Buffer: ["buffer", "Buffer"],
+      // }),
+      new webpack.NormalModuleReplacementPlugin(/node:/, (resource) => {
+        const mod = resource.request.replace(/^node:/, "");
+        switch (mod) {
+          case "buffer":
+            resource.request = "buffer";
+            break;
+          case "stream":
+            resource.request = "readable-stream";
+            break;
+          default:
+            throw new Error(`Not found ${mod}`);
+        }
+      })
+    ],
+
+    // node: {
+    //   fs: 'empty',
+    //   net: 'empty',
+    //   tls: 'empty'
+    // },
+    }
 })
