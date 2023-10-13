b654fa068e697e5e35d9c24f8acb2152b2a495d3
Kristian Garza <kj.garza@gmail.com>
2023-10-13 10:48:34 +0200

    Create ProteinVisualizer.vue

---
+++ src/components/ProteinVisualizer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="viewport" style="width: 100%; height: 400px;"></div>
+</template>
+
+<script>
+import * as NGL from 'ngl';
+
+export default {
+  name: 'ProteinVisualizer',
+  mounted() {
+    this.initVisualization();
+  },
+  beforeUnmount() {
+    window.removeEventListener("resize", this.handleResize);
+  },
+  methods: {
+    initVisualization() {
+      // Create NGL Stage object
+      this.stage = new NGL.Stage("viewport");
+
+      // Handle window resizing
+      window.addEventListener("resize", this.handleResize, false);
+
+      // Load PDB entry 1CRN
+      this.stage.loadFile("rcsb://1crn", { defaultRepresentation: true });
+    },
+    handleResize() {
+      this.stage.handleResize();
+    }
+  }
+};
+</script>
